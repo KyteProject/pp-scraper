47cae57f1a4c2e76f3843f14bbc763d1b7e5f773
KyteProject <headhertz88@gmail.com>
2019-04-18 14:45:53 +0100

    working chart data/value access

--- app.js
+++ app.js
@@ -5,125 +5,131 @@ import puppeteer from 'puppeteer';
 
 ( async() => {
   const branchExclusion = [ 'All', 'Cornerstone Wide', 'Operational Support', 'Training Academy' ],
-    teamExclusion = [ 'All' ],
+    teamExclusion = require( './excludedTeams.js' ),
     allowedShiftTypes = [ 'Support', 'Support Share-Lead' ],
     date = '08/04/2019',
     data = [],
     browser = await puppeteer.launch( { headless: false } ),
     page = await browser.newPage();
 
-  await page.setViewport( { width: 1024, height: 738 } );
-  await page.goto( 'https://www.peopleplanner.biz/Security/Login.aspx' );
-  await page.type( '#txtAccountID', process.env.DB );
-  await page.click( '#btnLogin' );
-  await page.waitFor( 1000 );
-  await page.click( '#btnLogin' );
+  try {
+    await page.setViewport( { width: 1024, height: 738 } );
+    await page.goto( 'https://www.peopleplanner.biz/Security/Login.aspx' );
+    await page.type( '#txtAccountID', process.env.DB );
+    await page.click( '#btnLogin' );
+    await page.waitFor( 1000 );
+    await page.click( '#btnLogin' );
+  } catch ( err ) {
+    console.log( err );
+  }
 
   // Handle popup window
-  const [ popup ] = await Promise.all( [
-    new Promise( ( resolve ) => page.once( 'popup', resolve ) ),
-    console.log( 'popupwindow' )
-  ] );
+  try {
+    const [ popup ] = await Promise.all( [
+      new Promise( ( resolve ) => page.once( 'popup', resolve ) ),
+      console.log( 'Focused popup.' )
+    ] );
 
-  await popup.setViewport( { width: 1024, height: 738 } );
-  await popup.waitForSelector( '.LoginSSMButton' );
-  await popup.click( '.LoginSSMButton' );
-  await popup.waitForSelector( '#txtUserCode' );
-  await popup.type( '#txtUserCode', process.env.USER );
-  await popup.type( '#txtUserPassword', process.env.PASSWORD );
-  await popup.click( '#btnLogin' );
-  await popup.waitForSelector( '#Header_RadTabStrip1_ctl04' );
-  await popup.click( '#Header_RadTabStrip1_ctl04' );
-  await popup.waitForSelector( '#ifrmDetail' );
+    await popup.setViewport( { width: 1024, height: 738 } );
+    await popup.waitForSelector( '.LoginSSMButton' );
+    await popup.click( '.LoginSSMButton' );
+    await popup.waitForSelector( '#txtUserCode' );
+    await popup.type( '#txtUserCode', process.env.USER );
+    await popup.type( '#txtUserPassword', process.env.PASSWORD );
+    await popup.click( '#btnLogin' );
+    await popup.waitForSelector( '#Header_RadTabStrip1_ctl04' );
+    await popup.click( '#Header_RadTabStrip1_ctl04' );
+    await popup.waitForSelector( '#ifrmDetail' );
 
-  // Enter iframe
-  const frameUrl = await popup.evaluate( () => {
-    const url = document.querySelector( '#ifrmDetail' );
+    // Enter iframe
+    const frameUrl = await popup.evaluate( () => {
+      const url = document.querySelector( '#ifrmDetail' );
 
-    return url.src;
-  } );
-
-  await popup.goto( frameUrl );
-
-  // Main logic
-  const branches = await popup.evaluate( ( branchExclusion ) => {
-    const options = [];
-
-    document.querySelectorAll( '#ddlBranch option' ).forEach( ( opt ) => {
-      if ( !branchExclusion.includes( opt.textContent ) ) {
-        options.push( { name: opt.textContent, value: opt.value } );
-      }
+      return url.src;
     } );
 
-    return options;
-  }, branchExclusion );
+    await popup.close();
+    await page.goto( frameUrl );
 
-  await popup.type( '#txtFromDate', date );
-  await popup.select( '#ddlStatus', '1' );
+    console.log( 'Focused inner iframe' );
+  } catch ( err ) {
+    console.log( err );
+  }
 
-  for ( let i = 0; i < branches.length; i++ ) {
-    await popup.select( '#ddlBranch', branches[ i ].value );
-    await popup.waitFor( 1000 );
-    await popup.select( '#ddlBranch', branches[ i ].value );
-    await popup.waitFor( 3000 );
-
-    const teams = await popup.evaluate( ( teamExclusion ) => {
+  // Main logic
+  try {
+    const branches = await page.evaluate( ( branchExclusion ) => {
       const options = [];
 
-      document.querySelectorAll( '#ddlBranchArea option' ).forEach( ( opt ) => {
-        if ( !teamExclusion.includes( opt.textContent ) ) {
+      document.querySelectorAll( '#ddlBranch option' ).forEach( ( opt ) => {
+        if ( !branchExclusion.includes( opt.textContent ) ) {
           options.push( { name: opt.textContent, value: opt.value } );
         }
       } );
 
       return options;
-    }, teamExclusion );
+    }, branchExclusion );
 
-    for ( let x = 0; x < teams.length; x++ ) {
-      await popup.select( '#ddlBranchArea', teams[ x ].value );
-      await popup.waitFor( 1500 );
-      await popup.waitForSelector( '#PlanningServiceTypeChartContainer' );
-      const aHandle = await popup.evaluateHandle( () => window.Highcharts.charts[ 1 ].series[ 0 ].data ),
-        handleProps = await aHandle.getProperties(),
-        propsSize = handleProps.size;
+    await page.type( '#txtFromDate', date );
+    await page.select( '#ddlStatus', '1' );
 
-      for ( let y = 0; y < propsSize; y++ ) {
-        const props = await aHandle.getProperty( y.toString() );
+    for ( let i = 0; i < branches.length; i++ ) {
+      await page.select( '#ddlBranch', branches[ i ].value );
+      await page.waitFor( 1000 );
+      await page.select( '#ddlBranch', branches[ i ].value );
+      await page.waitFor( 3000 );
 
-        let type = await props.getProperty( 'category' ),
-          total = await props.getProperty( 'total' ),
-          percentage = await props.getProperty( 'percentage' );
+      const teams = await page.evaluate( ( teamExclusion ) => {
+        const options = [];
 
-        type = await type.jsonValue();
-        total = await total.jsonValue();
-        percentage = await percentage.jsonValue();
+        document.querySelectorAll( '#ddlBranchArea option' ).forEach( ( opt ) => {
+          if ( !teamExclusion.list.includes( opt.textContent ) ) {
+            options.push( { name: opt.textContent, value: opt.value } );
+          }
+        } );
 
-        const unallocated = Math.round( total * percentage / 100 ),
-          allocated = total - unallocated;
+        return options;
+      }, teamExclusion );
 
-        console.log( props );
+      for ( let x = 0; x < teams.length; x++ ) {
+        await page.select( '#ddlBranchArea', teams[ x ].value );
+        await page.waitFor( 1500 );
+        await page.select( '#ddlBranchArea', teams[ x ].value );
+        await page.waitForSelector( '#PlanningServiceTypeChartContainer' );
+
+        const index = await page.evaluate( () => {
+            return parseInt(
+              document
+                .querySelector( '#PlanningServiceTypeChartContainer' )
+                .getAttribute( 'data-highcharts-chart' )
+            );
+          } ),
+          aHandle = await page.evaluateHandle( ( index ) => {
+            return window.Highcharts.charts[ index ].series[ 0 ].data;
+          }, index ),
+          props = await aHandle.getProperties();
+
+        for ( let y = 0; y < props.size; y++ ) {
+          const props = await aHandle.getProperty( y.toString() );
+
+          let type = await props.getProperty( 'category' ),
+            total = await props.getProperty( 'total' ),
+            percentage = await props.getProperty( 'percentage' );
+
+          type = await type.jsonValue();
+          total = await total.jsonValue();
+          percentage = await percentage.jsonValue();
+
+          const unallocated = Math.round( total * percentage / 100 ),
+            allocated = total - unallocated;
+
+          // filter only support types
+          // sum
+          // push data to global write object
+        }
       }
     }
+  } catch ( err ) {
+    console.log( err );
   }
-
-  // const jsHandle = await popup.evaluateHandle( () => {
-  //     const chart = window.Highcharts.charts[ 1 ].series[ 0 ].data;
-
-  //     console.log( chart );
-  //     return chart;
-  //   } ),
-  //   chartData = await popup.evaluate( async( jsHandle ) => {
-  //     jsHandle.forEach.forEach( ( serviceType ) => {
-  //       console.log( `${serviceType.category}: ${serviceType.total} ${serviceType.percentage}%` );
-
-  //       // if ( allowedShiftTypes.includes( serviceType.category ) ) {
-  //       //   const shiftData = {
-  //       //     type: serviceType.category,
-  //       //     unallocated: Math.round( serviceType.total * serviceType.percentage / 100 )
-  //       //   };
-
-  //       //   shiftData.allocated = serviceType.total - shiftData.unallocated;
-  //       // }
-  //     } );
-  //   }, jsHandle );
 } )();
